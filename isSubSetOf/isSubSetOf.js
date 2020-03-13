a5b082a00085e291fdfcb306ffade0e86e137c34
abdellihou <abdelli.hou@gmail.com>
2020-03-13 03:10:05 -0700

    ToyProblem

--- isSubSetOf/isSubSetOf.js
+++ isSubSetOf/isSubSetOf.js
@@ -19,6 +19,34 @@
 /*
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
 */
-Array.prototype.isSubsetOf = function (arr) {
-    // your code here
-};
+// Array.prototype.isSubsetOf = function (arr) {
+//     // your code here
+// };
+
+
+function isSubsetOf (arr1,arr2) {
+    var newArray=[];
+    var arr=[];
+    //this loop looks for the element present in the two arrays
+       for (var i=0 ; i<arr2.length ; i++) {
+            for (var j=0 ; j<arr1.length ; j++ ) {
+            if (arr2[i]===arr1[j]) {
+            newArray.push(arr2[i])
+             }
+            }
+    }
+    //this if tests if the array1 is a subset of array2
+       if (newArray.length === arr1.length) {
+           for (var i=0 ; i<newArray.length ; i++) {
+               for (var j=newArray.length ; j<=0 ; j--) {
+               if (newArray[i]===newArray[j]) {    //these statements disregard duplicates 
+                   arr.push(newArray[i])
+                }
+               }
+           }
+           return true
+       }
+       else {
+           return false
+       }
+}
