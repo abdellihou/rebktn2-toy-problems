0cf27f336c7ead73d195df77f776e3d11faa8b20
ABDELLI Houssem Eddine <abdelli.hou@gmail.com>
2020-03-19 09:52:12 +0100

    Abdelli

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,21 +7,35 @@ The queue should not have any storage separate from its stacks.
 
 var Stack = function() {
   var storage = [];
+  var size=0;
 
-  this.push = function() {};
+  this.push = function(value) {
+    storage.push(value)
+    size++
+  };
 
-  this.pop = function() {};
+  this.pop = function() {
+    storage.pop()
+    size--
+  };
 
-  this.size = function() {};
+  this.size = function() {
+    return size
+  };
 };
 
 var Queue = function() {
   var inbox = new Stack();
   var outbox = new Stack();
 
-  this.enqueue = function() {};
+  this.enqueue = function(item) {
+    inbox.push(item)
+    size++
+  };
 
   this.dequeue = function() {};
 
-  this.size = function() {};
+  this.size = function() {
+    
+  };
 };
